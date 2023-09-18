8ba1f0cfb62e6574caadda5dcc884d86d57b0540
sbux1992 <sinsunkangg@naver.com>
2023-09-18 16:52:02 +0900

    2023-09-18 프로젝트 생성

---
+++ wherego/src/main/java/com/wherego/WheregoApplication.java
@@ -0,0 +1,13 @@
+package com.wherego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheregoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheregoApplication.class, args);
+	}
+
+}
---
+++ wherego/src/test/java/com/wherego/WheregoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wherego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheregoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
