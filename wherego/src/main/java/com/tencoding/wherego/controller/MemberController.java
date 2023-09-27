af87038837e2c45e046c27f2a89df1e6ec3b6637
kmh9250x <kmh9250@gmail.com>
2023-09-27 17:52:39 +0900

    0927 로그인 로직, 카카오로그인 진행중

--- wherego/src/main/java/com/tencoding/wherego/controller/MemberController.java
+++ wherego/src/main/java/com/tencoding/wherego/controller/MemberController.java
@@ -1,27 +1,57 @@
 package com.tencoding.wherego.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
+import com.tencoding.wherego.dto.LogInFormDto;
 import com.tencoding.wherego.dto.SignUpFormDto;
+import com.tencoding.wherego.repository.model.Member;
 import com.tencoding.wherego.service.MemberService;
+import com.tencoding.wherego.utils.Define;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
-	
+
 	@Autowired
 	private MemberService memberService;
-	
+	@Autowired
+	private HttpSession session;
+
 	@GetMapping("/login")
 	public String login() {
 		return "member/login";
 	}
 
-	@GetMapping("/sign_up")
+	@PostMapping("/login")
+	@ResponseBody // 메인페이지가 생기면 교체
+	public Member loginProc(LogInFormDto logInFormDto) {
+		// TODO : 유효성검사
+
+		Member principal = memberService.logIn(logInFormDto);
+		principal.setMemPassword(null);
+		// 3. 쿠키 + 세션
+		session.setAttribute(Define.PRINCIPAL, principal);
+
+		return principal;
+//		return "member/login";
+	}
+
+	@GetMapping("/sign-up")
 	public String signUp() {
 		return "member/signUp";
 	}
@@ -29,10 +59,41 @@ public class MemberController {
 	@PostMapping("/sign_up")
 	public String signUpProc(SignUpFormDto signUpFormDto) {
 		// TODO : 유효성검사
-		
+
 		memberService.signUp(signUpFormDto);
-		
+
 		return "redirect:/member/login";
 	}
+
+	@GetMapping("/kakao/callback")
+	@ResponseBody // 이녀셕은 data 반환 명시
+	public String kakaoCallback(@RequestParam String code) {
+		System.out.println("메서드 동작");
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "ae138cb481abfa831e99189dc79e66b1");
+		params.add("redirect_uri", "http://localhost:8080/wherego/member/kakao/callback");
+		params.add("code", code);
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		// HttpEntity 결합 (헤더 + 바디)
+		HttpEntity<MultiValueMap<String, String>> reqMes = new HttpEntity<>(params, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange("https://kauth.kakao.com/oauth/token", HttpMethod.POST,
+				reqMes, String.class);
+
+		return response.toString();
+	}
+
+//	@GetMapping("/search")
+//	public String search() {
+//		System.out.println(22222222);
+//		return "member/search";
+//	}
 
 }
--- wherego/src/main/webapp/WEB-INF/view/member/login.jsp
+++ wherego/src/main/webapp/WEB-INF/view/member/login.jsp
@@ -13,19 +13,30 @@
 
 <body>
 	<div class="container" style="width: 600px;">
-		<h2>Where Go!</h2>
-		<form action="/action_page.php">
+		<h2 style="text-align: center;">로그인</h2>
+		<form action="/wherego/member/login" method="post">
 			<div class="form-group">
-				<label for="mem_id">ID :</label> <input type="text" class="form-control" id="mem_id" placeholder="Enter ID" name="mem_id" style="width: 500px">
+				<input type="text" class="form-control" id="id" placeholder="아이디를 입력해주세요" name="id" style="margin: 0 auto; width: 400px; height: 45px">
 			</div>
-			<div class="form-group">
-				<label for="mem_password">Password :</label> <input type="password" class="form-control" id="mem_password" placeholder="Enter password" name="mem_password" style="width: 500px">
+			<div class="form-group" style="margin: 0 auto; width: 400px">
+				<input type="password" class="form-control" id="password" placeholder="비밀번호를 입력해주세요" name="password" style="margin: 0 auto; width: 400px; height: 45px">
 			</div>
-			<div class="form-group form-check">
-				<label class="form-check-label"> <input class="form-check-input" type="checkbox" name="remember"> 로그인 기억하기
+			<div class="form-group form-check" style="margin: 15px auto; width: 400px">
+				<label class="form-check-label"> <input class="form-check-input" type="checkbox" name="remember">로그인 기억하기
 				</label>
 			</div>
-			<button type="submit" class="btn btn-primary" style="width: 500px">Submit</button>
+			<div class="form-group">
+				<button type="submit" class="btn btn-primary" style="display: block; margin: 0 auto; width: 400px; height: 45px;">로그인</button>
+			</div>
+
+			<div class="form-group">
+				<button type="button" class="btn btn-outline-primary" onclick="location.href='sign-up'" style="display: block; margin: 0 auto; width: 400px; height: 45px">회원가입</button>
+			</div>
+			<div class="form-group" style="margin: 15px auto; width: 400px; display: flex;">
+				<a href="https://kauth.kakao.com/oauth/authorize?client_id=ae138cb481abfa831e99189dc79e66b1&redirect_uri=http://localhost:8080/wherego/member/kakao/callback&response_type=code"><img
+					src="${pageContext.request.contextPath}/img/kakao_login_medium.png" alt="" style="margin: 0 auto;"> </a> <a
+					style="display: inline-block; width: 192px; height: 45px; border: #000 1px solid; box-sizing: border-box; margin-left: 16px">소셜로그인 하나 더 예정</a>
+			</div>
 		</form>
 	</div>
 </body>
