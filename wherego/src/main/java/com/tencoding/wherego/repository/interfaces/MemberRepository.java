0db8a662c80babaaa193ef97363f775231497867
kmh9250x <kmh9250@gmail.com>
2023-09-27 18:12:10 +0900

    0927 로그인, 카카오로그인

--- wherego/src/main/java/com/tencoding/wherego/repository/interfaces/MemberRepository.java
+++ wherego/src/main/java/com/tencoding/wherego/repository/interfaces/MemberRepository.java
@@ -2,10 +2,20 @@ package com.tencoding.wherego.repository.interfaces;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.tencoding.wherego.dto.LogInFormDto;
 import com.tencoding.wherego.dto.SignUpFormDto;
+import com.tencoding.wherego.repository.model.Member;
 
 @Mapper
 public interface MemberRepository {
 	// 회원가입
 	public int insert(SignUpFormDto signUpFormDto);
+	// 로그인
+	public Member findByIdAndPassword(LogInFormDto logInFormDto);
+	// 회원정보 수정
+	
+	// 아이디 찾기
+	
+	// 비밀번호 찾기
+	
 }
--- wherego/src/main/java/com/tencoding/wherego/service/MemberService.java
+++ wherego/src/main/java/com/tencoding/wherego/service/MemberService.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tencoding.wherego.dto.LogInFormDto;
 import com.tencoding.wherego.dto.SignUpFormDto;
 import com.tencoding.wherego.repository.interfaces.MemberRepository;
+import com.tencoding.wherego.repository.model.Member;
 
 @Service
 public class MemberService {
@@ -15,9 +17,19 @@ public class MemberService {
 
 	@Transactional
 	public void signUp(SignUpFormDto signUpFormDto) {
-		// TODO 비밀번호 해시처리, 가입실패시 익셉션처리
+		// TODO 비밀번호 해시처리, 가입실패시 익셉션처리(유효성)
 		int result = memberRepository.insert(signUpFormDto);
-		
+
 		System.out.println(result);
+	}
+
+	public Member logIn(LogInFormDto logInFormDto) {
+		Member memberEntity = memberRepository.findByIdAndPassword(logInFormDto);
+		// TODO 유효성검사
+		if (memberEntity == null || memberEntity.getMemId().equals(logInFormDto.getId()) == false) {
+			System.out.println("계정이 없음");
+		}
+		
+		return memberEntity;
 	}
 }
